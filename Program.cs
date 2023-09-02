edf6bf0510ae7432bd7019c0e9b44aa6ee61c753
Aysegul ESKI <ayseguleskk@gmail.com>
2023-09-02 20:33:48 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int x=9;
+        int y=15;
+        Console.WriteLine(x+y);
+
+        int sonuc=Topla(x,y);
+        Console.WriteLine(sonuc);
+
+        int sonuc2=ArttirveTopla(x,y);
+        EkranaYazdir(Convert.ToString(sonuc2));
+        EkranaYazdir(Convert.ToString(x+y));
+    }
+
+    public static int Topla(int deger1, int deger2)
+    {
+        return deger1 + deger2;
+    }
+
+    public static void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public static int ArttirveTopla(int deger1, int deger2)
+    {
+        deger1 += 1;
+        deger2 +=1;
+        return deger1+deger2;
+    }
+}
